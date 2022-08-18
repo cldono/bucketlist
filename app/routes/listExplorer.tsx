18b6a86b503ef885598dab8305091b4793174ede
cldono <c.donovan605@gmail.com>
2022-08-18 14:41:24 -0400

    rm commented out code

--- app/routes/listExplorer.tsx
+++ app/routes/listExplorer.tsx
@@ -54,35 +54,6 @@ export default function ListExplorer() {
 
   return (
     <div>
-      {/* <div className="inline-flex rounded-md shadow-sm" role="group">
-        <button type="button" className="py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-l-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
-            Profile
-        </button>
-        <button type="button" className="py-2 px-4 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
-            Settings
-        </button>
-        <button type="button" className="py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-r-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
-            Messages
-        </button>
-        </div> */}
-      {/* <form>
-                <div className="flex flex-row gap-3 align-center">
-                    <label className="flex-none">Filter your list:</label>
-                    <select className="flex-none" name="searchColumn" id="cars">
-                        <option  className={inputClass} key={"first"}></option>
-                        {COLUMN_HEADERS.map((header) => (
-                            <option key={header.slug} value={header.slug}>{header.display}</option>
-                        ))}
-                    </select>
-
-                
-                    <input className={"flex-none appearance-none block bg-gray-200 text-gray-700 border rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"} name="searchTerm" type="text" placeholder="Spain"/>
-                    <button type="submit" className="flex-none rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
-                        Go
-                    </button>
-                    <Link to="/listExplorer">Clear</Link>
-                </div>
-            </form> */}
       <div className="flex items-center gap-5">
         <EventsTable events={events} />
         <Outlet />
